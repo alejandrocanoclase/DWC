fb5a1d76791d0fc9a3e5a8de0c777c567c0cbc9a
AlejandroCano <alejandrocano36@gmail.com>
2022-10-04 19:12:40 +0200

    Proyecto buscaminas V2

--- BUSCAMINAS/js/buscaminas.js
+++ BUSCAMINAS/js/buscaminas.js
@@ -2,7 +2,7 @@ let maxFilas = prompt('¿Cuántas filas quieres?');
 let maxColumnas = prompt('¿Cuántas columnas quieres?');
 let numMinas = prompt('¿Cuántas minas quieres introducir?')
 
-
+/*
 // Creamos el tablero en html
 document.write('<table>');
 
@@ -16,16 +16,35 @@ for (let i = 0; i < maxFilas; i++) {
     document.write('</tr>');
 }
 document.write('</table>');
-
+*/
 // Array bidimensional para guardar las minas
 
 let arrayTablero = [];
+let contadorMinas = 0;
+let posFila;
+let posColumna;
 
-for (let mina = 0; mina < numMinas; mina++){
-    posFila= Math.floor(Math.random()*maxFilas);
-    console.log(posFila)
-    //arrayTablero[posFila][posColumna] = "MINA"
+for (let fila = 0; fila < maxFilas; fila++) {
+    arrayTablero[fila] = [];
+
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        arrayTablero[fila][columna] = '';
+        
+    }
 }
+
+while (contadorMinas < numMinas) {
+    posFila = Math.floor(Math.random() * maxFilas);
+    posColumna = Math.floor(Math.random() * maxColumnas);
+
+    if (arrayTablero[posFila][posColumna] != 'MINA') {
+        arrayTablero[posFila][posColumna] = 'MINA'
+        contadorMinas++;
+    }
+}
+
+console.log(arrayTablero)
+console.log(contadorMinas)
 
 
 
