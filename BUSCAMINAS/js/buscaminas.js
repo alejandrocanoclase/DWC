358c82cf9326ab265a82875a61bb23100fb7ed76
AlejandroCano <alejandrocano36@gmail.com>
2022-10-07 17:05:31 +0200

    buscaminas ahí ahí

--- BUSCAMINAS/js/buscaminas.js
+++ BUSCAMINAS/js/buscaminas.js
@@ -1,9 +1,10 @@
 let maxFilas = prompt('¿Cuántas filas quieres?');
 let maxColumnas = prompt('¿Cuántas columnas quieres?');
-let numMinas = prompt('¿Cuántas minas quieres introducir?')
+let numMinas = prompt('¿Cuántas minas quieres introducir?');
 
-/*
+
 // Creamos el tablero en html
+
 document.write('<table>');
 
 for (let i = 0; i < maxFilas; i++) {
@@ -16,72 +17,58 @@ for (let i = 0; i < maxFilas; i++) {
     document.write('</tr>');
 }
 document.write('</table>');
-*/
-// Array bidimensional para guardar las minas
 
+
+// Crear array bidimensional para guardar las minas
 let arrayTablero = [];
 let contadorMinas = 0;
-let posFila;
-let posColumna;
 
 for (let fila = 0; fila < maxFilas; fila++) {
-    arrayTablero[fila] = [];
+    arrayTablero[fila] = new Array(maxColumnas);
 
     for (let columna = 0; columna < maxColumnas; columna++) {
         arrayTablero[fila][columna] = '';
+    }
+}
+
+
+let posFila;
+let posColumna;
+
+
+while (contadorMinas < numMinas) {
+    posFila = Math.floor(Math.random()*maxFilas);
+    posColumna = Math.floor(Math.random()*maxColumnas);
+
+    if (arrayTablero[posFila][posColumna] != 'MINA') {
+        arrayTablero[posFila][posColumna] = 'MINA';
+        contadorMinas++ ;
+    };
+  };
+
+
+let numMinasAlrededor;
+
+for (let fila = 0; fila < maxFilas; fila++) {
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        numMinasAlrededor = 0;
+        if (arrayTablero[fila][columna] != 'MINA'){
+            for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
+                for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
+                    if ((cFila >= 0 && cFila < maxFilas) && (cColumna >= 0 && cColumna < maxColumnas)) {
+                        if (arrayTablero[cFila][cColumna] == 'MINA') {
+                            
+                            numMinasAlrededor++;
+                        }
+                    } 
+                }
+            }
+            arrayTablero[fila][columna] = numMinasAlrededor;
+        }
         
     }
 }
-/*
-while (contadorMinas < numMinas) {
-    posFila = Math.floor(Math.random() * maxFilas);
-    posColumna = Math.floor(Math.random() * maxColumnas);
-
-    if (arrayTablero[posFila][posColumna] != 'MINA') {
-        arrayTablero[posFila][posColumna] = 'MINA'
-        contadorMinas++;
-    }
-}
-*/
-
-let F = 1
-let C = 1
-
-arrayTablero[F][C] = 'MINA'
-
-if (arrayTablero[F][C] == 'MINA'){
-
-    for (let posiciones = 0;posiciones < 3;posiciones++) {
-
-        if (arrayTablero[F-1][C] == null){
-            continue
-        }else{
-            arrayTablero[F-1][posiciones] = 1
-        }
-       
-        if (arrayTablero[F+1][C] === null){
-            continue
-        }else{
-            arrayTablero[F+1][posiciones] = 1
-        }
-       
-    }
-    if (arrayTablero[F][C-1] != null){
-        arrayTablero[1][C-1] = 1
-    }
-
-    if (arrayTablero[F][C+1] != null){
-        arrayTablero[1][C+1] = 1
-    }
-    
-    
-
-}
-
-console.log(arrayTablero)
-//console.log(contadorMinas)
 
 
-
-
+console.log(arrayTablero);
 
