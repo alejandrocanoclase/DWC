7f2747db4e4dba6d1891a513ac379edafeb9386d
AlejandroCano <alejandrocano36@gmail.com>
2022-10-14 16:51:17 +0200

    código en funciones

--- BUSCAMINAS/js/buscaminas.js
+++ BUSCAMINAS/js/buscaminas.js
@@ -3,70 +3,87 @@ let maxColumnas = prompt('¿Cuántas columnas quieres?');
 let numMinas = prompt('¿Cuántas minas quieres introducir?');
 
 
-// Creamos el tablero en html
-
-document.write('<table>');
-
-for (let i = 0; i < maxFilas; i++) {
-    document.write('<tr>');
-
-    for (let j = 0; j < maxColumnas; j++) {
-        document.write('<td></td>');
-    }
-
-    document.write('</tr>');
-}
-document.write('</table>');
-
 
 // Crear array bidimensional para guardar las minas
 let arrayTablero = [];
-let contadorMinas = 0;
 
-for (let fila = 0; fila < maxFilas; fila++) {
-    arrayTablero[fila] = new Array(maxColumnas);
 
-    for (let columna = 0; columna < maxColumnas; columna++) {
-        arrayTablero[fila][columna] = '';
+function crearArray(arrayTablero) {
+
+    for (let fila = 0; fila < maxFilas; fila++) {
+        arrayTablero[fila] = new Array(maxColumnas);
+
+        for (let columna = 0; columna < maxColumnas; columna++) {
+            arrayTablero[fila][columna] = '';
+        }
     }
+
 }
 
 
-let posFila;
-let posColumna;
+function colocarMinas() {
+    let posFila;
+    let posColumna;
+    let contadorMinas = 0;
 
+    while (contadorMinas < numMinas) {
+        posFila = Math.floor(Math.random() * maxFilas);
+        posColumna = Math.floor(Math.random() * maxColumnas);
 
-while (contadorMinas < numMinas) {
-    posFila = Math.floor(Math.random() * maxFilas);
-    posColumna = Math.floor(Math.random() * maxColumnas);
-
-    if (arrayTablero[posFila][posColumna] != 'MINA') {
-        arrayTablero[posFila][posColumna] = 'MINA';
-        contadorMinas++;
+        if (arrayTablero[posFila][posColumna] != 'MINA') {
+            arrayTablero[posFila][posColumna] = 'MINA';
+            contadorMinas++;
+        };
     };
-};
+}
 
 
-let numMinasAlrededor;
 
-for (let fila = 0; fila < maxFilas; fila++) {
-    for (let columna = 0; columna < maxColumnas; columna++) {
-        numMinasAlrededor = 0;
-        if (arrayTablero[fila][columna] != 'MINA') {
-            for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
-                if (cFila >= 0 && cFila < maxFilas) {
-                    for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
-                        if (cColumna >= 0 && cColumna < maxColumnas &&
-                            arrayTablero[cFila][cColumna] == 'MINA') {
-                            numMinasAlrededor++;
+function minasAlrededor() {
+    let numMinasAlrededor;
+    for (let fila = 0; fila < maxFilas; fila++) {
+        for (let columna = 0; columna < maxColumnas; columna++) {
+            numMinasAlrededor = 0;
+            if (arrayTablero[fila][columna] != 'MINA') {
+                for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                    if (cFila >= 0 && cFila < maxFilas) {
+                        for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                            if (cColumna >= 0 && cColumna < maxColumnas &&
+                                arrayTablero[cFila][cColumna] == 'MINA') {
+                                numMinasAlrededor++;
+                            }
                         }
                     }
+                    arrayTablero[fila][columna] = numMinasAlrededor;
                 }
-                arrayTablero[fila][columna] = numMinasAlrededor;
-            }
 
+            }
         }
     }
 }
 
-    console.log(arrayTablero);
+//console.log(arrayTablero);
+
+// Creamos el tablero en html
+
+function pintarTablero(arrayTablero, maxFilas, maxColumnas) {
+
+    document.write('<table>');
+
+    for (let i = 0; i < maxFilas; i++) {
+        document.write('<tr>');
+
+        for (let j = 0; j < maxColumnas; j++) {
+            document.write('<td>', arrayTablero[i][j], '</td>');
+        }
+
+        document.write('</tr>');
+    }
+    document.write('</table>');
+
+}
+
+crearArray(arrayTablero);
+colocarMinas();
+minasAlrededor();
+pintarTablero(arrayTablero, maxFilas, maxColumnas);
