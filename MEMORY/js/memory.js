f7964571a3b86a24629c541aba34d3ff8530e0c7
Alejandro <alejandrocano36@gmail.com>
2022-10-24 17:50:19 +0200

    dibujar tablero

--- MEMORY/js/memory.js
+++ MEMORY/js/memory.js
@@ -1,46 +1,66 @@
 class Tablero{
     
-    constructor(filas,columnas){
-        this.filas = filas;
-        this.columnas = columnas;
+    filas;
+    columnas;
 
+    constructor(){
+        
+        this.tamañoTablero();
+        this.esParTablero();
         this.crearTablero();
+        
 
     }
 
-    
 
-    crearTablero() {
-        this.arrayTablero = [];
-       
+    tamañoTablero(){
+        this.filas = parseInt(prompt("¿Cuántas filas quieres en el tablero?"));
+        this.columnas = parseInt(prompt("¿Cuántas columnas quieres en el tablero?"));
+    }
 
-
-        for (let i = 0; i < this.filas; i++) {
-            this.arrayTablero[i] = [];
-            console.log("fila ", i);
-
-            for (let j = 0; j < this.columnas; j++) {
-                console.log("columna ", i);
-                this.arrayTablero[i][j] = 'hola';
+    esParTablero(){
+        let par = false;
+        while (par != true){
+            if (this.filas * this.columnas % 2 != 0){
+                alert("El tablero no es par con los valores que has introucido \n Por favor, cambialos.");
+                this.tamañoTablero();
+            }else{
+                par = true;
             }
         }
-
-        for (let i = 0; i < this.filas; i++) {
-            for (let j = 0; j < this.columnas; j++) {
-                console.log(this.arrayTablero[i][j]);                
-            }
-        }
-
         
     }
 
+    crearTablero() {
+        this.arrayTablero = [];
 
+        for (let fila = 0; fila < this.filas; fila++) {
+            this.arrayTablero[fila] = [];
 
+            for (let columna = 0; columna < this.columnas; columna++) {
+                this.arrayTablero[fila][columna] = '';
+            }
+        }
+    }
 
-    getFilas(){
-        return this.filas;
+    dibujarTablero() {
+        // Creamos el tablero en html
+        document.write('<table>');
+
+        for (let i = 0; i < this.filas; i++) {
+            document.write('<tr>');
+
+            for (let j = 0; j < this.columnas; j++) {
+                document.write(`<td>${this.arrayTablero[i][j]}</td>`);
+                console.log(this.arrayTablero[i][j]);
+            }
+
+            document.write('</tr>');
+        }
+        document.write('</table>');
     }
 
 }
-let tableroMaster = new Tablero(5,2);
+
+
 
