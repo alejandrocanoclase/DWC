eb39977ba5af4c274c0e1a04ce536296085a3cf5
alehtrus <alejandrocano36@gmail.com>
2022-12-07 19:08:53 +0100

    no funciona el 0

--- BUSCAMINAS/js/buscaminas_obj.js
+++ BUSCAMINAS/js/buscaminas_obj.js
@@ -152,6 +152,65 @@ class Buscaminas extends Tablero {
         console.log(this.arrayTablero);
 
     }
+    despejar2(){
+        let fila = celda.dataset.fila;
+        let columna = celda.dataset.columna;
+
+        let contenido = this.arrayTablero[fila][columna];
+
+        //alert(contenido);
+        //console.log(this.arrayTablero);
+
+
+        if (contenido > 0 && contenido < 9) {
+            celda.innerHTML = contenido;
+            celda.removeEventListener('click', this.despejar.bind(this));
+            celda.removeEventListener('contextmenu', this.marcar);
+        } else if (contenido == 'MINA') {
+            celda.innerHTML = contenido;
+            alert('perdiste por mamahuevo');
+
+            let elemento;
+            for (let i = 0; i < this.filas; i++) {
+                for (let j = 0; j < this.columnas; j++) {
+                    elemento = document.getElementById("f" + i + "c" + j);
+
+                    if (elemento.innerHTML == "🚩" && this.arrayTablero[i][j] != 'MINA') {
+                        elemento.style.backgroundColor = "pink";
+                    }
+                    if (this.arrayTablero[i][j] == 'MINA') {
+                        elemento.innerHTML = this.arrayTablero[i][j];
+                        elemento.style.backgroundColor = "orange";
+                    }
+                }
+            }
+        } else if (contenido == 0) {
+            //alert(celda.id + " " + this.filas);
+            let elemento;
+            celda.innerHTML = contenido;
+            
+            let cFilas = parseInt(celda.dataset.fila);
+            console.log(typeof cFilas);
+            let cColumna = parseInt(celda.dataset.columna);
+            console.log(typeof cColumna);
+
+            for (let i = cFilas - 1; i <= cFilas + 1; i++) {
+                if (i >= 0 && i < this.filas) {
+                    for (let j = cColumna -1; j <= cColumna +1; j++){
+                        if(j >= 0 && j <= this.columnas){
+                            elemento = document.getElementById("f" + i + "c" + j);
+                            if (this.arrayTablero[i][j] !='MINA' && this.arrayTablero != 0){
+                                elemento.innerHTML= this.arrayTablero[i][j];
+                            }else if(this.arrayTablero[i][j] == 0){
+                                this.despejar2();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
     despejar(elEvento) {
         let evento = elEvento || window.event;
@@ -198,14 +257,14 @@ class Buscaminas extends Tablero {
             console.log(typeof cColumna);
 
             for (let i = cFilas - 1; i <= cFilas + 1; i++) {
-                if (i >= 0 && i <= this.filas) {
+                if (i >= 0 && i < this.filas) {
                     for (let j = cColumna -1; j <= cColumna +1; j++){
                         if(j >= 0 && j <= this.columnas){
                             elemento = document.getElementById("f" + i + "c" + j);
                             if (this.arrayTablero[i][j] !='MINA' && this.arrayTablero != 0){
                                 elemento.innerHTML= this.arrayTablero[i][j];
-                            }else if(this.arrayTablero[i][j] = 0){
-                                this.despejar();
+                            }else if(this.arrayTablero[i][j] == 0){
+                                this.despejar2();
                             }
                         }
                     }
@@ -213,6 +272,8 @@ class Buscaminas extends Tablero {
             }
         }
     }
+
+    
 
     marcar() {
         window.oncontextmenu = function () {
